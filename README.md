7fcdca2e666f847a51497180660c433011f2a94e
Richard Goshen <rick.goshen@gmail.com>
2022-07-15 17:43:26 -0700

    -m

--- README.md
+++ README.md
@@ -121,6 +121,51 @@ const greeting = (state = defaultState, action) => {
 
 ## Properties On Actions & In Reducers
 
+_src/App.js_
+
+```javascript
+import { createStore } from 'redux';
+import './App.css';
+
+function App() {
+  return <div className='App'></div>;
+}
+
+export default App;
+
+const defaultState = {
+  welcome: 'Hi',
+  otherState: 'some stuff',
+};
+
+// reducer function
+const greeting = (state = defaultState, action) => {
+  switch (action.type) {
+    case 'GREET_NAME':
+      return { ...state, welcome: `Hello ${action.name}` };
+    case 'GREET_WORLD':
+      return { ...state, welcome: 'Hello World' };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(greeting);
+console.log(store.getState());
+
+const name = 'Rick';
+
+// action object (describer)
+store.dispatch({
+  type: 'GREET_NAME',
+  name,
+});
+
+console.log(store.getState());
+```
+
+![output](assets/images/output_2.png)
+
 [toc](#toc)
 
 ## Adding Redux to Our React App
--- redux-test/src/App.js
+++ redux-test/src/App.js
@@ -1,24 +1,24 @@
-import { createStore } from "redux";
-import "./App.css";
+import { createStore } from 'redux';
+import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return <div className='App'></div>;
 }
 
 export default App;
 
 const defaultState = {
-  welcome: "Hi",
-  otherState: "some stuff",
+  welcome: 'Hi',
+  otherState: 'some stuff',
 };
 
 // reducer function
 const greeting = (state = defaultState, action) => {
   switch (action.type) {
-    case "GREET_ME":
-      return { ...state, welcome: "Hello Rick" };
-    case "GREET_WORLD":
-      return { ...state, welcome: "Hello World" };
+    case 'GREET_NAME':
+      return { ...state, welcome: `Hello ${action.name}` };
+    case 'GREET_WORLD':
+      return { ...state, welcome: 'Hello World' };
     default:
       return state;
   }
@@ -27,9 +27,12 @@ const greeting = (state = defaultState, action) => {
 const store = createStore(greeting);
 console.log(store.getState());
 
+const name = 'Rick';
+
 // action object (describer)
 store.dispatch({
-  type: "GREET_ME",
+  type: 'GREET_NAME',
+  name,
 });
 
 console.log(store.getState());
