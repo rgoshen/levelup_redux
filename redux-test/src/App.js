821165471be542f200f63344c178cbe64f0e5804
Richard Goshen <rick.goshen@gmail.com>
2022-07-15 17:26:05 -0700

    first lint action on whole project

--- redux-test/src/App.js
+++ redux-test/src/App.js
@@ -1,24 +1,24 @@
-import { createStore } from 'redux';
-import './App.css';
+import { createStore } from "redux";
+import "./App.css";
 
 function App() {
-  return <div className='App'></div>;
+  return <div className="App"></div>;
 }
 
 export default App;
 
 const defaultState = {
-  welcome: 'Hi',
-  otherState: 'some stuff',
+  welcome: "Hi",
+  otherState: "some stuff",
 };
 
 // reducer function
 const greeting = (state = defaultState, action) => {
   switch (action.type) {
-    case 'GREET_ME':
-      return { ...state, welcome: 'Hello Rick' };
-    case 'GREET_WORLD':
-      return { ...state, welcome: 'Hello World' };
+    case "GREET_ME":
+      return { ...state, welcome: "Hello Rick" };
+    case "GREET_WORLD":
+      return { ...state, welcome: "Hello World" };
     default:
       return state;
   }
@@ -29,7 +29,7 @@ console.log(store.getState());
 
 // action object (describer)
 store.dispatch({
-  type: 'GREET_ME',
+  type: "GREET_ME",
 });
 
 console.log(store.getState());
