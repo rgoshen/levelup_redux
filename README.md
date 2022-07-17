ec98d6e0b390ad30186b7c36960c064ee36c2b50
Richard Goshen <rick.goshen@gmail.com>
2022-07-16 20:14:19 -0700

    -m

--- README.md
+++ README.md
@@ -623,6 +623,66 @@ export default function (state = initialState, action) {
 
 ## Adding Middleware Redux Logger
 
+`npm i redux-logger`
+
+_src/App.js_
+
+```javascript
+/* eslint react/no-did-mount-set-state: 0 */
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
+
+
+import logo from './logo.svg';
+import './App.css';
+
+import rootReducer from './rootReducer';
+
+import MoviesList from './MoviesList';
+import MovieDetail from './MovieDetail';
+import Toggle from './Toggle';
+
+const middleware = [logger];
+
+const store = createStore(
+  rootReducer,
+  {},
+  composeWithDevTools(applyMiddleware(...middleware)),
+);
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/">
+            <img src={logo} className="App-logo" alt="logo" />
+          </Link>
+        </header>
+        <Toggle />
+        <Switch>
+          <Route exact path="/" component={MoviesList} />
+          <Route path="/:id" component={MovieDetail} />
+        </Switch>
+      </div>
+    </Router>
+  </Provider>
+);
+
+export default App;
+```
+
+![logger output](assets/images/logger_ouput.png)
+
 [toc](#toc)
 
 ## Redux Thunks and API Calls for Actions
--- redux-movielist/src/App.js
+++ redux-movielist/src/App.js
@@ -1,14 +1,10 @@
 /* eslint react/no-did-mount-set-state: 0 */
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Link,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
 
 import logo from './logo.svg';
 import './App.css';
@@ -19,25 +15,27 @@ import MoviesList from './MoviesList';
 import MovieDetail from './MovieDetail';
 import Toggle from './Toggle';
 
+const middleware = [logger];
+
 const store = createStore(
   rootReducer,
   {},
-  composeWithDevTools(),
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 const App = () => (
   <Provider store={store}>
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <Link to="/">
-            <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <header className='App-header'>
+          <Link to='/'>
+            <img src={logo} className='App-logo' alt='logo' />
           </Link>
         </header>
         <Toggle />
         <Switch>
-          <Route exact path="/" component={MoviesList} />
-          <Route path="/:id" component={MovieDetail} />
+          <Route exact path='/' component={MoviesList} />
+          <Route path='/:id' component={MovieDetail} />
         </Switch>
       </div>
     </Router>
